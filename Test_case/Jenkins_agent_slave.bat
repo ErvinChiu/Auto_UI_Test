48bcb75c35136e11aa7c8251e40484676c354df6
ErvinChiu <ervinchiu@outlook.com>
2020-11-17 18:53:59 +0800

    自动化UI更新，添加失败再运行机制

---
+++ Test_case/Jenkins_agent_slave.bat
@@ -0,0 +1,1 @@
+java -jar agent.jar -jnlpUrl http://60.205.194.47:8080/computer/Js_AutoUI_Test/slave-agent.jnlp -secret 960884f21fb018fe11e4ee0bc4cc4d276354732be6e059b7bcdeae2510bed06f -workDir "D:\PycharmProjects\JS_UIAuto_Test"
