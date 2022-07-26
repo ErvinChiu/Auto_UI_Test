1d0e4fb2a2b15c6e3c0db496ac84738a94aa00ff
ErvinChiu <47777194+ErvinChiu@users.noreply.github.com>
2022-07-26 15:59:51 +0800

    Update run.py

--- Test_case/run.py
+++ Test_case/run.py
@@ -1,6 +1,6 @@
 import unittest
 from unittestreport import TestRunner
-from Test_case.Test_Case import Test_JS_Cases
+from Test_Case import Test_JS_Cases
 suite=unittest.defaultTestLoader.loadTestsFromTestCase(Test_JS_Cases)
 runner=TestRunner(suite=suite)
 runner.run
